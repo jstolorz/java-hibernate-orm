65edc159fbbd31b61bc0e15bbf2b4582b31c0292
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-04-27 10:36:25 +0200

    Initial commit

---
+++ src/main/java/com/bluesoft/javahibernateorm/JavaHibernateOrmApplication.java
@@ -0,0 +1,13 @@
+package com.bluesoft.javahibernateorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHibernateOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHibernateOrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluesoft/javahibernateorm/JavaHibernateOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluesoft.javahibernateorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHibernateOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
